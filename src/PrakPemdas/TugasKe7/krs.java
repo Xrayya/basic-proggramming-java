a73301431159711aa4ac20a2942eb20da4f01099
Xrayya <nur.alkhiyar@gmail.com>
2021-11-08 15:32:01 +0700

    menambahkan isi krs.java

--- src/PrakPemdas/TugasKe7/krs.java
+++ src/PrakPemdas/TugasKe7/krs.java
@@ -5,5 +5,16 @@ import java.util.Scanner;
 public class krs {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String nama, nim, prodi;
+        String namaMatkul[] = new String[5];
+        byte sks[] = new byte[5];
+
+        System.out.println("Masukkan nama mahasiswa: ");
+        nama = in.nextLine();
+        System.out.println("Masukkan NIM mahasiswa: ");
+        nim = in.next();
+        System.out.println("Masukkan program studi mahasiswa: ");
+        prodi = in.nextLine();
+
     }
 }
