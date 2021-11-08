06f97d7fc3134dc83b33daf4b3fa396b96dd8e8b
Xrayya <nur.alkhiyar@gmail.com>
2021-11-08 12:41:45 +0700

    menambah file khs.java di folder TugasKe7

---
+++ src/PrakPemdas/TugasKe7/khs.java
@@ -0,0 +1,10 @@
+package PrakPemdas.TugasKe7;
+
+import java.util.Scanner;
+
+public class khs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
