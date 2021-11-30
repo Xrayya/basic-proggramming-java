0cff07560bd09070b32782c69cc5cfccb7e20dfc
Xrayya <nur.alkhiyar@gmail.com>
2021-11-30 10:50:54 +0700

    menyelesaikan registerLogin

--- src/PrakPemdas/HR10/registerLogin.java
+++ src/PrakPemdas/HR10/registerLogin.java
@@ -5,49 +5,128 @@ import java.util.Scanner;
 public class registerLogin {
     static int MAX_SLOT = 5;
 
-    static String[] usrname = new String[MAX_SLOT];
-    static String[] pwd     = new String[MAX_SLOT];
-    static String[] name    = new String[MAX_SLOT];
+    static String[] username = new String[MAX_SLOT];
+    static String[] password = new String[MAX_SLOT];
+    static String[] name     = new String[MAX_SLOT];
+    static int availSlot;
 
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        workflow();
     }
 
     static void workflow() {
-        menu();
+        String cmd;
+
+        do {
+            availSlot = MAX_SLOT - findEmptyindex();
+            menu();
+            cmd = in.nextLine();
+                switch (cmd) {
+                    case "1":
+                        if (availSlot != 0) {
+                            register();
+                        } else {
+                            login();
+                        }
+                        break;
+                    case "2":
+                        if (availSlot != 0) {
+                            login();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+        } while (!cmd.equals("0"));
     }
 
     static void menu() {
-        int availSlot, beginIndex;
-        availSlot = MAX_SLOT - emptyindex();
+        int beginIndex;
 
         System.out.printf("Slot akun = %d\n", availSlot);
         System.out.println("Daftar Perintah");
-        if (availSlot == 0) {
+        if (availSlot != 0) {
             beginIndex = 1;
-            System.out.printf("%d. Register", beginIndex);
+            System.out.printf("%d. Register\n", beginIndex);
         } else {
             beginIndex = 0;
         }
-        System.out.printf("%d. Login", beginIndex + 1);
-        System.out.printf("0. Berhenti");
-        System.out.print("Masukkan perintah: ");
+        System.out.printf("%d. Login\n", beginIndex + 1);
+        System.out.printf("0. Berhenti\n");
+        System.out.println("Masukkan perintah: ");
     }
 
     static void register() {
-        ;
+        int emptyIndex = findEmptyindex();
+
+        System.out.println("Masukkan username: ");
+        username[emptyIndex] = in.nextLine();
+        System.out.println("Masukkan password: ");
+        password[emptyIndex] = in.nextLine();
+        System.out.println("Masukkan nama: ");
+        name[emptyIndex] = in.nextLine();
     }
 
     static void login() {
-        ;
+        String usrname, pwd;
+        int usrIndex;
+        boolean usrnameExist, pwdMatch;
+
+        if (availSlot == 5) {
+            System.out.println("Belum ada akun!");
+        } else {
+            do {
+                System.out.println("Masukkan username: ");
+                usrname = in.nextLine();
+                System.out.println("Masukkan password: ");
+                pwd = in.nextLine();
+
+                usrIndex = findUsernameIndex(usrname);
+                if (usrIndex == MAX_SLOT) {
+                    usrnameExist = false;
+                    pwdMatch = false;
+                } else {
+                    usrnameExist = true;
+                    if (pwd.equals(password[usrIndex])) {
+                        pwdMatch = true;
+                    } else {
+                        pwdMatch = false;
+                    }
+                }
+
+                if (!usrnameExist) {
+                    System.out.println("Username Salah!");
+                } else {
+                    if (!pwdMatch) {
+                        System.out.println("Password Salah!");
+                    }
+                }
+
+                if (usrnameExist && pwdMatch) {
+                    System.out.printf("Username: %s\n", username[usrIndex]);
+                    System.out.printf("Nama: %s\n", name[usrIndex]);
+                }
+            } while (!usrnameExist || !pwdMatch);
+        }
     }
 
-    static int emptyindex() {
+    static int findEmptyindex() {
         int i = 0;
         while (i < MAX_SLOT) {
-            if (pwd[i] == null) {
+            if (password[i] == null) {
+                break;
+            }
+            i++;
+        }
+        return i;
+    }
+
+    static int findUsernameIndex(String usrname) {
+        int i = 0;
+        while (i < MAX_SLOT) {
+            if (usrname.equals(username[i])) {
                 break;
             }
             i++;
