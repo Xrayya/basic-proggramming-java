b4a23aa42eb1ea71cd3b4a824b43bf4decaaffad
Xrayya <nur.alkhiyar@gmail.com>
2021-12-01 07:55:33 +0700

    memulai ubjekFinal

---
+++ src/PrakPemdas/TugasKe9Latihan8/UbjekFinal.java
@@ -0,0 +1,11 @@
+package PrakPemdas.TugasKe9Latihan8;
+
+import java.util.Scanner;
+
+public class UbjekFinal {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
